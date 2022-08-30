42f743aaefcbcb565b3b77901c6978261df00b1b
Cole Peters <cole@colepeters.com>
2022-08-30 11:58:31 -0500

    Fix rendering of items

--- app/elements/todos-list.mjs
+++ app/elements/todos-list.mjs
@@ -2,14 +2,15 @@ export default function TodosList({ html, state = {} }) {
   const { store = {} } = state
   const { todos = [] } = store
 
-  return todos.map(
-    (todo) =>
-      html`
-        <todo-item
-          id=${todo.id}
-          label=${todo.label}
-          checked=${todo.checked}
-        ></todo-item>
-      `
+  const items = todos.map(
+    (todo) => `
+      <todo-item
+        id=${todo.id}
+        label=${todo.label}
+        checked=${todo.checked}
+      ></todo-item>
+    `
   )
+
+  return html`${items.join('')}`
 }
